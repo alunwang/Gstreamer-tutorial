10304c2ea557a240416e337d071793e3c920721b
Lunsheng Wang <lunsheng.wang@atlasaero.space>
2023-08-11 14:55:49 +0300

    Enhanced 3rd tutorial to add video stream to demux

--- src/basic_tutorial_3.py
+++ src/basic_tutorial_3.py
@@ -13,22 +13,35 @@ logger = logging.getLogger(__name__)
 
 
 class Player(object):
-    '''Dynamically build a pipeline for a player'''
+    '''
+    Dynamically build a pipeline for a player as below:
+    gst-launch-1.0 uridecodebin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm  name=demux demux. \
+        ! videoconvert ! autovideosink demux. \
+        ! audioconvert ! audioresample ! autoaudiosink
+    '''
     def __init__(self, uri="https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm"):
+        '''
+        Main function for building the pipeline and run it
+        '''
         Gst.init(sys.argv[1:])
 
         self.source = Gst.ElementFactory.make("uridecodebin", "source")
-        self.convert = Gst.ElementFactory.make("audioconvert", "convert")
+        self.audioconvert = Gst.ElementFactory.make("audioconvert", "audioconvert")
+        self.videoconvert = Gst.ElementFactory.make("videoconvert", "videoconvert")
         self.resample = Gst.ElementFactory.make("audioresample", "resample")
-        self.sink = Gst.ElementFactory.make("autoaudiosink", "sink")
+        self.audiosink = Gst.ElementFactory.make("autoaudiosink", "audiosink")
+        self.videosink = Gst.ElementFactory.make("autovideosink", "videosink")
 
-        self.pipeline = Gst.Pipeline.new("test-pipeline")
-        if not self.source or not self.convert or not self.resample or not self.sink or not self.pipeline:
+        self.pipeline = Gst.Pipeline.new("player-pipeline")
+        if not self.source or not self.audioconvert or not self.resample or not self.audiosink \
+            or not self.videoconvert or not self.videosink or not self.pipeline:
             logger.error("Not all elements could be created.")
             sys.exit(1)
 
-        self.pipeline.add(self.source, self.convert, self.resample, self.sink)
-        if not self.convert.link(self.resample) or not self.resample.link(self.sink):
+        self.pipeline.add(self.source, self.audioconvert, self.resample, self.audiosink, \
+                          self.videoconvert, self.videosink)
+        if not self.audioconvert.link(self.resample) or not self.resample.link(self.audiosink) \
+            or not self.videoconvert.link(self.videosink):
             logger.error("Elements could not be linked.")
             sys.exit(1)
 
@@ -40,6 +53,7 @@ class Player(object):
             logger.error("Unable to set the pipeline to the playing state.")
             sys.exit(1)
 
+        # Message processing from the bus
         bus = self.pipeline.get_bus()
         terminate = False
         while True:
@@ -74,23 +88,30 @@ class Player(object):
         self.pipeline.set_state(Gst.State.NULL)
 
     def on_pad_added(self, src, new_pad):
-        sink_pad = self.convert.get_static_pad("sink")
-        logger.info(f"Received new pad {new_pad.get_name()} \
-                    from {src.get_name()}")
-        
-        if (sink_pad.is_linked()):
+        '''
+        Callback for pad-added signal to add corresponding pad
+        '''
+        audio_sink_pad = self.audioconvert.get_static_pad("sink")
+        video_sink_pad = self.videoconvert.get_static_pad("sink")
+        if (audio_sink_pad.is_linked() and video_sink_pad.is_linked()):
             logger.info("We are already linked. Ignoring.")
             return
-        
+
+        logger.info(f"Received new pad {new_pad.get_name()} \
+                    from {src.get_name()}")
+
         new_pad_caps = new_pad.get_current_caps()
         new_pad_struct = new_pad_caps.get_structure(0)
         new_pad_type = new_pad_struct.get_name()
 
-        if not new_pad_type.startswith("audio/x-raw"):
-            logger.info(f"It has type {new_pad_type} which is not raw audio. Ignoring.")
+        if not new_pad_type.startswith("audio/x-raw") and not new_pad_type.startswith("video/x-raw"):
+            logger.info(f"It has type {new_pad_type} which is not raw audio/video. Ignoring.")
             return
-        
-        ret = new_pad.link(sink_pad)
+        if new_pad_type.startswith("audio/x-raw"):
+            ret = new_pad.link(audio_sink_pad)
+        else: # video/x-raw
+            ret = new_pad.link(video_sink_pad)
+
         if not ret == Gst.PadLinkReturn.OK:
             logger.info(f"Type is {new_pad_type} but link failed.")
         else:
