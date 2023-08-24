664ad846276fd3b4b9a67e4a558fad9e6580c2a6
Lunsheng Wang <lunsheng.wang@atlasaero.space>
2023-08-24 11:12:34 +0300

    Add the helper which formats nanosecond to hh:mm:ss:ns

---
+++ src/helper.py
@@ -0,0 +1,9 @@
+def format_ns(ns):
+    '''
+    Format nanosecond into hh:mm:ss:ns
+    '''
+    s, ns = divmod(ns, 1000000000)
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+
+    return "%u:%02u:%02u.%09u" % (h, m, s, ns)
